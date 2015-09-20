1bedcf5ef6344bf8f1a4cf55efa22e972c91312c
BobbyJoeSmith3 <bobbyjoe@codeforprogress.org>
2015-09-20 02:59:12 +0200

    Add buttons to start new game and decide range.

--- guess-the-number.py
+++ guess-the-number.py
@@ -9,20 +9,22 @@ def new_game():
     # initialize global variables used in your code here
     global secret_number
     secret_number = random.randrange(0, 100)
-    print "the secret number is", secret_number
 
 
 # define event handlers for control panel
 def range100():
     # button that changes the range to [0,100) and starts a new game
-
-    # remove this when you add your code
-    pass
+    new_game()
+    global secret_number
+    secret_number = random.randrange(0, 100)
+    print "New game. Range is [0,100)"
 
 def range1000():
     # button that changes the range to [0,1000) and starts a new game
-
-    pass
+    new_game()
+    global secret_number
+    secret_number = random.randrange(0, 1000)
+    print "New game. Range is [0,1000)"
 
 def input_guess(guess):
     # convert user input to interger
@@ -47,10 +49,14 @@ frame = simplegui.create_frame('Guess The Number', 300, 300)
 
 
 # register event handlers for control elements and start frame
+# new game buttons
+zero_to_one_hundred = frame.add_button('Range is [0,100)', range100, 150)
+zero_to_one_thousand = frame.add_button('Range is [0,1000)', range1000, 150)
 
 # input field for player guesses
-inp = frame.add_input('Enter Guess', input_guess, 100)
+inp = frame.add_input('Enter Guess', input_guess, 150)
 
+# start all frame event handlers
 frame.start()
 
 # call new_game
