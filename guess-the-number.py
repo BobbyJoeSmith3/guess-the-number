00b8922e4ed6a4de9cc43a000dab95240a6ab053
BobbyJoeSmith3 <bobbyjoe@codeforprogress.org>
2015-09-20 04:47:42 +0200

    Tell player when they ran out of guesses and start a new game.

--- guess-the-number.py
+++ guess-the-number.py
@@ -16,12 +16,10 @@ def new_game():
     print "New game. Range is 0 to", num_range
     # tell player how many guesses they have left
     print "Number of remaining guesses is", num_of_guesses
-    # decrement the number of guesses player has left
-    num_of_guesses = num_of_guesses - 1
     # create space between entries
     print ""
-
-
+    # decrement the number of guesses player has left
+    num_of_guesses = num_of_guesses - 1
 
 # define event handlers for control panel
 def range100():
@@ -42,37 +40,32 @@ def input_guess(guess):
     global num_of_guesses
     # convert user input to interger
     user_input = int(guess)
-
     # print user's guess to console and number of guesses left
     print "Guess was", user_input
     print "Number of remaining guesses is", num_of_guesses
-
     # main game logic goes here
-    if secret_number > user_input:
+    if num_of_guesses == 0:
+        print "You ran out of guesses. The number was", secret_number
+        print ""
+        new_game()
+    elif secret_number > user_input:
         print "Higher"
     elif secret_number < user_input:
         print "Lower"
     elif secret_number == user_input:
         print "Correct"
-
     # decrement the number of guesses player has left
     num_of_guesses = num_of_guesses - 1
-
     # create space between entries
     print ""
 
-
-
-
 # create frame
 frame = simplegui.create_frame('Guess The Number', 300, 300)
-
 
 # register event handlers for control elements and start frame
 # new game buttons
 zero_to_one_hundred = frame.add_button('Range is [0,100)', range100, 150)
 zero_to_one_thousand = frame.add_button('Range is [0,1000)', range1000, 150)
-
 # input field for player guesses
 inp = frame.add_input('Enter Guess', input_guess, 150)
 
